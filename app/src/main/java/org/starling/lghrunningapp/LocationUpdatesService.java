656758c1fc98b993a217f9be87fcf2d6bd903cc4
eddyspreeuwers <e.spreeuwers@gmail.com>
2020-11-15 15:29:20 +0100

    initial

---
+++ app/src/main/java/org/starling/lghrunningapp/LocationUpdatesService.java
@@ -0,0 +1,178 @@
+package org.starling.lghrunningapp;
+
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.location.Location;
+import android.os.Build;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+//import androidx.NotificationCompat;
+import android.util.Log;
+
+import static org.starling.lghrunningapp.MainActivity.MESSENGER_INTENT_KEY;
+
+
+/**
+ * location update service continues to running and getting location information
+ */
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class LocationUpdatesService extends JobService implements LocationUpdatesComponent.ILocationProvider {
+
+    private static final String TAG = LocationUpdatesService.class.getSimpleName();
+    public static final int LOCATION_MESSAGE = 9999;
+    private static final int NOTIFICATION_ID = 1;
+    private static final String SERVICE_CHANNEL_ID = "SERVICE_CHANNEL_GPS";
+    private final NotificationChannel test = createServiceChannel("test");
+
+    private Messenger mActivityMessenger;
+
+    private LocationUpdatesComponent locationUpdatesComponent;
+    //public static List<Location> locations = new ArrayList<>();
+
+    public LocationUpdatesService() {
+    }
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        Log.i(TAG, "onStartJob....");
+//        Utils.scheduleJob(getApplicationContext(), LocationUpdatesService.class);
+        return true;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        Log.i(TAG, "onStopJob....");
+
+        locationUpdatesComponent.onStop();
+
+        return false;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "created...............");
+
+        locationUpdatesComponent = new LocationUpdatesComponent(this);
+
+        locationUpdatesComponent.onCreate(this);
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "onStartCommand Service started");
+        if (intent != null) {
+            mActivityMessenger = intent.getParcelableExtra(MESSENGER_INTENT_KEY);
+        }
+        //hey request for location updates
+        locationUpdatesComponent.onStart();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            startAsForeground();
+        else
+            startForeground(1, new Notification());
+
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, SERVICE_CHANNEL_ID)
+//        .setContentTitle("GPS")
+//        .setContentText("bg gps subscription running ");
+//        Notification notification = builder.build();
+//
+//
+//        startForeground(NOTIFICATION_ID, notification);
+        return START_STICKY;
+    }
+
+    private void startAsForeground(){
+        String NOTIFICATION_CHANNEL_ID = this.getClass().getCanonicalName();
+        String channelName = "My Background Service";
+        NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+        chan.setLightColor(Color.BLUE);
+        chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        assert manager != null;
+        manager.createNotificationChannel(chan);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        Notification notification = notificationBuilder.setOngoing(true)
+                //.setSmallIcon(R.drawable.icon_1)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+        startForeground(NOTIFICATION_ID, notification);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public static NotificationChannel createServiceChannel(CharSequence name) {
+        NotificationChannel channel = new NotificationChannel(SERVICE_CHANNEL_ID, name, NotificationManager.IMPORTANCE_LOW);
+        channel.enableVibration(false);
+        return channel;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        // Called when a client (MainActivity in case of this sample) returns to the foreground
+        // and binds once again with this service. The service should cease to be a foreground
+        // service when that happens.
+        Log.i(TAG, "in onRebind()");
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.i(TAG, "Last client unbound from service");
+
+        return true; // Ensures onRebind() is called when a client re-binds.
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "onDestroy....");
+
+    }
+
+    /**
+     * send message by using messenger
+     *
+     * @param messageID
+     */
+    private void sendMessage(int messageID, Location location) {
+        // If this service is launched by the JobScheduler, there's no callback Messenger. It
+        // only exists when the MainActivity calls startService() with the callback in the Intent.
+        if (mActivityMessenger == null) {
+            Log.d(TAG, "Service is bound, not started. There's no callback to send a message to.");
+            return;
+        }
+
+        Message m = Message.obtain();
+        m.what = messageID;
+        m.obj = location;
+        try {
+            mActivityMessenger.send(m);
+        } catch (RemoteException e) {
+            Log.e(TAG, "Error passing service object back to activity.");
+        }
+    }
+
+    @Override
+    public void onLocationUpdate(Location location) {
+
+        sendMessage(LOCATION_MESSAGE, location);
+    }
+}
---
+++ app/src/main/java/org/starling/lghrunningapp/MainActivity.java
@@ -0,0 +1,133 @@
+package org.starling.lghrunningapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
+    public static final String MESSENGER_INTENT_KEY = "msg-intent-key";
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private IncomingMessageHandler mHandler;
+
+
+    private TextView locationMsg;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView2);
+
+        locationMsg = findViewById(R.id.textView1);
+
+        mHandler = new IncomingMessageHandler();
+
+
+        requestPermissions();
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                Log.i(TAG, "User interaction was cancelled.");
+                finish();
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // can be schedule in this way also
+                //  Utils.scheduleJob(this, LocationUpdatesService.class);
+                //doing this way to communicate via messenger
+                // Start service and provide it a way to communicate with this class.
+                Intent startServiceIntent = new Intent(this, LocationUpdatesService.class);
+                Messenger messengerIncoming = new Messenger(mHandler);
+                startServiceIntent.putExtra(MESSENGER_INTENT_KEY, messengerIncoming);
+                startService(startServiceIntent);
+            } else {
+                // Permission denied.
+                finish();
+            }
+        }
+    }
+
+    class IncomingMessageHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            Log.i(TAG, "handleMessage..." + msg.toString());
+
+            super.handleMessage(msg);
+
+            switch (msg.what) {
+                case LocationUpdatesService.LOCATION_MESSAGE:
+                    Location obj = (Location) msg.obj;
+                    String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+
+                    Object[] timestamps = LocationUpdatesComponent.timestamps.toArray();
+                    for (int i = 0; i < timestamps.length-1; i++){
+                        long t2 = (long)timestamps[i+1];
+                        long t1 = (long)timestamps[i];
+                        timestamps[i] = t2 - t1;
+                    }
+                    Long[] difs = {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L,0L};
+                    for (int i = 0; i < difs.length-1; i++){
+                        int x = timestamps.length -i -1;
+                        if (x < 0) break;
+                        difs[i] = (long) timestamps[x];
+
+                    }
+
+
+                    textView.setText("" + LocationUpdatesComponent.locations.size() + "\n"  + Arrays.toString(difs));
+
+                    locationMsg.setText("LAT :  " + obj.getLatitude() + "\nLNG : " + obj.getLongitude() + "\n\n" + obj.toString() + " \n\n\nLast updated- " + currentDateTimeString);
+                    break;
+            }
+        }
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // Provide an additional rationale to the user. This would happen if the user denied the
+        // request previously, but didn't check the "Don't ask again" checkbox.
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+            // Request permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        } else {
+            Log.i(TAG, "Requesting permission");
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+}
